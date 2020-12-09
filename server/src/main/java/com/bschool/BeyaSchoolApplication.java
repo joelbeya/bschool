d4bdef182a0a178a403643faeae20ecf66dcb14f
joelbeya <joelbeya.bj@gmail.com>
2020-12-09 03:50:27 +0100

    Initial push of client server

---
+++ server/src/main/java/com/bschool/BeyaSchoolApplication.java
@@ -0,0 +1,13 @@
+package com.bschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeyaSchoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeyaSchoolApplication.class, args);
+    }
+
+}
---
+++ server/src/test/java/com/bschool/BeyaSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeyaSchoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
