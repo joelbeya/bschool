75d195c229152db9c7be069f197cd1ddf9ad440b
joelbeya <joelbeya.bj@gmail.com>
2020-12-17 15:55:16 +0100

    some missing components changes

--- client/src/app/dashboard/book/book.module.ts
+++ client/src/app/dashboard/book/book.module.ts
@@ -2,11 +2,13 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {BookRoutingModule} from './book-routing.module';
 import {BookComponent} from './book.component';
+import {SharedModule} from '../../shared/shared.module';
 
 @NgModule({
   declarations: [BookComponent],
   imports: [
     CommonModule,
+    SharedModule,
     BookRoutingModule,
   ]
 })
